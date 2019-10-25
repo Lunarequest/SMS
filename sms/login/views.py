e9468323578089e8a45d38f267b9b39b23da55d9
advaith <advaith.madhukar@gmail.com>
2019-10-25 19:14:59 +0530

    got login working & moved to virtualenv

---
+++ sms/login/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.models import auth, User
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request,user)
+            return render(request, 'management.html')
+        else:
+            messages.info(request,'invaild login')
+            return redirect("/login")
+    else:
+        return render(request,'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect("login")
