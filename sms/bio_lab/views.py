2139d11a60b3daea69a06cafb7cae9de05eba306
advaith <advaith.madhukar@gmail.com>
2019-10-30 19:37:00 +0530

    staic folder

--- sms/bio_lab/views.py
+++ sms/bio_lab/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def console(request):
     response = HttpResponse('works')
+    response = render(request,'console.html')
     return response
