58b0da2971300544285d676fed4c48ead75bd43c
advaith <advaith.madhukar@gmail.com>
2019-10-30 19:03:31 +0530

    Update urls.py

--- sms/sms/urls.py
+++ sms/sms/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
-    path('/bio', include('bio_lab.urls')),
+    path('bio', include('bio_lab.urls')),
 ]
