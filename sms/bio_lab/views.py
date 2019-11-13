f2e35b026a86f4973fcdcdc80709af4cf9efe7f1
advaith <advaith.madhukar@gmail.com>
2019-11-13 18:46:17 +0530

    added more prefrontend code

--- sms/bio_lab/views.py
+++ sms/bio_lab/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import chem
+
 # Create your views here.
 def console(request):
-    response = HttpResponse('works')
-    response = render(request,'console.html')
-    return response
+    if (request.method == 'POST'):
+        entries = chem.objects.all()
+        context = {'entries':entries}
+        #if request.user.has_perm() to check if user has permissions 
+        return HttpResponse("loads")
+        
+    else:
+        response = HttpResponse('works')
+        response = render(request,'console.html')
+        return response
