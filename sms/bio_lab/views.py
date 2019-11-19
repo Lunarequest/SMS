be02fe4a2d85759ce6991c1619484987e963ac5d
advaith <advaith.madhukar@gmail.com>
2019-11-19 18:00:06 +0530

    changed view.py

--- sms/bio_lab/views.py
+++ sms/bio_lab/views.py
@@ -3,12 +3,17 @@ from django.http import HttpResponse
 from .models import chem
 
 # Create your views here.
+
+
 def console(request):
-    if(request.user.has_perm('bio.can_view_chem')):
+    if request.user.has_perm('bio_lab.can_view_chem'):
+        print('works')
         istekler = chem.objects.all()
         return render(request,'console.html',locals())
+    else:
+        print('error')
 def edit(request):       
-    if(request.user.has_perm('bio.can_change_chem') and request.user.has_perm('bio.can_add_chem') and request.user.has_perm('bio.can_delete_chem')):
+    if(request.user.has_perm('bio_lab.can_change_chem') and request.user.has_perm('bio.can_add_chem') and request.user.has_perm('bio.can_delete_chem')):
         if (request.method == 'POST'):
              entries = chem.objects.all()
             #MyModel.objects.filter(pk=some_value).update(field1='some value')
