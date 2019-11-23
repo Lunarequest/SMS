322d8f817f9261d468558e4d4b9ac03413fe7f39
advaith <advaith.madhukar@gmail.com>
2019-11-23 17:52:44 +0530

    fixing errors

--- sms/sms/settings.py
+++ sms/sms/settings.py
@@ -80,6 +80,7 @@ MIDDLEWARE = [
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_csslint',
     'django_jenkins.tasks.run_sloccount'
 )
 
