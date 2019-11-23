44be4356dd64831d18663a63a7c0fa65f80a4751
advaith <advaith.madhukar@gmail.com>
2019-11-23 17:36:37 +0530

    intigration of jenkins

--- sms/bio_lab/views.py
+++ sms/bio_lab/views.py
@@ -10,7 +10,7 @@ def console(request):
     if request.user.groups.filter(name__in=['bio_member']):
         istekler = chem.objects.all()
         loc = 'biology lab'
-        return render(request,'console.html',locals())
+        return render(request,'bio_lab/console.html',locals())
     else:
         messages.info(request, "error 401 access denied")
         return redirect("/sel")
@@ -21,3 +21,5 @@ def edit(request):
              entries = chem.objects.all()
             #MyModel.objects.filter(pk=some_value).update(field1='some value')
              return HttpResponse("loads")
+        else:
+            return render(request,"bio_lab/edit.html")
--- sms/sms/settings.py
+++ sms/sms/settings.py
@@ -80,8 +80,6 @@ MIDDLEWARE = [
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
-    'django_jenkins.tasks.run_jslint',
-    'django_jenkins.tasks.run_csslint',
     'django_jenkins.tasks.run_sloccount'
 )
 
