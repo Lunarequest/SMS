a02e621d6b2e57ed2bc6cb43d5eeac75f1d28aa6
advaith <advaith.madhukar@gmail.com>
2019-11-30 09:24:20 +0530

    wokring on stuff

--- sms/bio_lab/views.py
+++ sms/bio_lab/views.py
@@ -9,32 +9,43 @@ def console(request):
     if request.user.groups.filter(name__in=['bio_member']):
         items = bio_eq.objects.all()
         loc = 'biology lab'
-        return render(request,'bio_lab/edit.html',locals())
+        return render(request,'bio_lab/console.html',locals())
     else:
         messages.info(request, "error 401 access denied")
         return redirect("/sel")
 
-def edit(request, bio_eq_id= None):       
+def broken(request, bio_eq_id= None):       
     if request.user.groups.filter(name__in=['bio_member']):
         item = get_object_or_404(bio_eq,pk=bio_eq_id)
         if(request.method == "POST"):
              broken=request.POST['broken']
-             student_id=request.POST['student_id']
              if (broken == "broken"):
                  item = bio_eq.objects.filter(pk=bio_eq_id)
-                 amount=item.amount
+                 student_id=request.POST['student_id']
+                 amount=item.bio_eq_amount
                  name = item.bio_eq_name
-                 bio_eq.objects.filter(pk=bio_eq_id).update(amount=amount-1)
+                 bio_eq.objects.filter(pk=bio_eq_id).update(bio_eq_amount=amount-1)
                  bio_broken_eq.object.create(bio_eq_id=bio_eq_id,student=student_id,bio_eq_name=name)
+
 
              return redirect("/bio")
         else:
+            
             return render(request,'bio_lab/edit_item.html')
        
        
-       
+def display(request):
+    items = bio_eq.objects.all()
+    context ={
+        'items':items
+    }
+    return render(request,'bio_lab/edit.html',context)
     
 
 
 def save(request):
+    return redirect("/bio")
+
+def delete(request, bio_eq_id):
+    bio_eq.objects.filter(pk=bio_eq_id).delete()
     return redirect("/bio")
