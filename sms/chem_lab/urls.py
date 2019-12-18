2d37226a3630c5d6d9e1473c31c195bcfc7d93e5
advaith <advaith.madhukar@gmail.com>
2019-12-18 18:46:05 +0530

    fixed shit

--- sms/chem_lab/urls.py
+++ sms/chem_lab/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.console, name='chem_lab'),
     path('/edit', views.edit, name="edit"),
     path('/add_con', views.add_con, name="add_chem"),
+    path('/add_eq', views.add_eq, name='add'),
     path('edit_con/<int:consumable_id>', views.edit_con, name="editcon"),
     path('edit_eq/<int:chem_eq_id>', views.edit_eq, name="editcon"),
     path('edit_eq/chem_eq_id', views.edit_eq, name='edit_eq'),
